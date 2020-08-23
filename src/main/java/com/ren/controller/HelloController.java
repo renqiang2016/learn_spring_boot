7ea7d93e9a67859d248a94e89d1bab9b82f0063d
renqiang <22talentren@sina.com>
2020-08-23 23:31:12 +0800

    start learn_spring_boot

---
+++ src/main/java/com/ren/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.ren.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author qiang.ren
+ * @version 1.0
+ * @since 2020/8/23 22:53
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("hello")
+    public String hello(){
+        return "<font style='font-size:28px;'>Hello Spring Boot</font>";
+    }
+}
