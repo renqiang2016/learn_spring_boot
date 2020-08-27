95edb0c0e84994acab2a58462830a0dbdc855ff0
renqiang <22talentren@sina.com>
2020-08-27 11:03:34 +0800

    部署war包

--- src/main/java/com/ren/LearnSpringBootApplication.java
+++ src/main/java/com/ren/LearnSpringBootApplication.java
@@ -2,12 +2,20 @@ package com.ren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearnSpringBootApplication {
+public class LearnSpringBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+	// 用于构建war文件并进行部署
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 
 }
